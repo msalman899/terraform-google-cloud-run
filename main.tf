8612718ca1c8e9d6ff3f433349cf3e4a57f7fa58
Muhammad Salman <m.salman899@gmail.com>
2025-10-07 18:28:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -198,6 +198,7 @@ resource "google_cloud_run_service" "main" {
       template[0].metadata[0].annotations["run.googleapis.com/client-name"],
       template[0].metadata[0].annotations["run.googleapis.com/client-version"],
       template[0].metadata[0].labels["client.knative.dev/nonce"],
+      template[0].spec[0].containers[0].image,
     ]
   }
 }
